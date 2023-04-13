b6abddec9ee181f5a44e5571e4854b21d920e1a9
Pratik Neupane <pratikneupane1999@gmail.com>
2023-04-13 23:32:04 +0545

    chore: added index in contants for publicroute

--- src/constants/publicRoutes.ts
+++ src/constants/publicRoutes.ts
@@ -1,4 +1,5 @@
 export const publicRoutes = [
+  '/',
   '/terms-conditions',
   '/login',
   '/register',
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,6 @@ const Home: NextPage = () => {
         <title>Hajurbuwa</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>OnBoarding</div>
     </div>
   );
 };
