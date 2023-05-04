abc1b531df5b5ba8a8ee6d90701bd611c25be39e
Bishal <bishal.k03@gmail.com>
2023-05-04 20:41:09 +0545

    fix: build error

--- src/components/Editor.tsx
+++
@@ -1,9 +0,0 @@
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import React from 'react';
-
-const Editor = () => {
-  return <CKEditor editor={ClassicEditor} data={'Hello mero vai'} />;
-};
-
-export default Editor;
