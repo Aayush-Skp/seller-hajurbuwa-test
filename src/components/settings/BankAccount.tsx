5ccb762dd99c72dc641ba10d501f419a63a8013b
Bishal <bishal.k03@gmail.com>
2023-05-07 08:15:29 +0545

    bank

--- src/components/settings/BankAccount.tsx
+++ src/components/settings/BankAccount.tsx
@@ -2,13 +2,11 @@ import React, { useEffect, useState } from 'react';
 import Button from '../common/Button';
 import InputLabel from '../common/InputLabel';
 import TextInput from '../common/TextInput';
-import useFormValidation from '../../hooks/useFormValidation';
-import { BankAccountSchema } from '../../validation/sellerProfileSchema';
-import ErrorMessage from '../common/ErrorMessage';
 import { getBankList } from '../../services/getBankList';
 import { addBankDetails } from '../../services/profileService';
 import { useRouter } from 'next/router';
 import Spinner from '../loader/Spinner';
+import ErrorMessage from '../common/ErrorMessage';
 
 export default function BankAccount() {
   const [bankList, setBankList] = useState<
@@ -17,14 +15,90 @@ export default function BankAccount() {
 
   const [isLoading, setIsLoading] = useState(false);
 
-  const { register, errors, handleSubmit, setValue, getValues } =
-    useFormValidation(BankAccountSchema(bankList));
+  const [selectedBank, setSelectedBank] = useState('');
+
+  const [accName, setAccName] = useState('');
+
+  const [accNo, setAccNo] = useState('');
+
+  const [bankValidation, setBankValidation] = useState({
+    isValid: true,
+    message: '',
+  });
+
+  const [accountNameValidation, setAccountNameValidation] = useState({
+    isValid: true,
+    message: '',
+  });
+
+  const [accountNoValidation, setAccountNoValidation] = useState({
+    isValid: true,
+    message: '',
+  });
 
   const router = useRouter();
 
-  function handleFormSubmit(data: any) {
+  function handleBankSelection(e: React.ChangeEvent<HTMLSelectElement>) {
+    setSelectedBank(e.target.value);
+    setBankValidation({
+      isValid: true,
+      message: '',
+    });
+  }
+
+  function handleAccNameChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setAccName(e.target.value);
+    setAccountNameValidation({
+      isValid: true,
+      message: '',
+    });
+  }
+
+  function handleAccNoChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setAccNo(e.target.value);
+    setAccountNoValidation({
+      isValid: true,
+      message: '',
+    });
+  }
+
+  function handleFormSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (selectedBank === '') {
+      setBankValidation({
+        isValid: false,
+        message: 'Please select a bank',
+      });
+
+      return;
+    }
+
+    if (accName === '') {
+      setAccountNameValidation({
+        isValid: false,
+        message: 'Please enter account name',
+      });
+
+      return;
+    }
+
+    if (accNo === '') {
+      setAccountNoValidation({
+        isValid: false,
+        message: 'Please enter account no.',
+      });
+
+      return;
+    }
+
     setIsLoading(true);
-    addBankDetails(data)
+
+    addBankDetails({
+      account_name: accName,
+      account_number: accNo,
+      bank_id: selectedBank,
+    })
       .then(() => {
         const value = localStorage.getItem('userDetails');
 
@@ -35,9 +109,9 @@ export default function BankAccount() {
             'userDetails',
             JSON.stringify({
               ...updatedValue,
-              account_name: getValues('account_name'),
-              account_number: getValues('account_number'),
-              bank_id: getValues('bank_id'),
+              account_name: accName,
+              account_number: accNo,
+              bank_id: selectedBank,
             })
           );
         }
@@ -59,9 +133,9 @@ export default function BankAccount() {
       .then(() => {
         if (typeof value === 'string') {
           const sellerDetails = JSON.parse(value);
-          setValue('account_name', sellerDetails?.account_name);
-          setValue('account_number', sellerDetails?.account_number);
-          setValue('bank_id', sellerDetails?.bank_id.toString());
+          setAccName(sellerDetails?.account_name);
+          setAccNo(sellerDetails?.account_number);
+          setSelectedBank(`${sellerDetails?.bank_id}`);
         }
       })
       .catch(console.log);
@@ -69,17 +143,14 @@ export default function BankAccount() {
 
   return (
     <section>
-      <form
-        onSubmit={handleSubmit(handleFormSubmit)}
-        className="flex flex-col space-y-5"
-      >
+      <form onSubmit={handleFormSubmit} className="flex flex-col space-y-5">
         <div className="flex space-x-10 items-center">
           <div className="w-36">
             <InputLabel className="text-xl" label="Account Name" />
           </div>
           <div className="w-80">
-            <TextInput {...register('account_name')} />
-            <ErrorMessage message={errors?.account_name?.message as string} />
+            <TextInput onChange={handleAccNameChange} value={accName} />
+            <ErrorMessage message={accountNameValidation?.message} />
           </div>
         </div>
         <div className="flex space-x-10 items-center">
@@ -87,8 +158,8 @@ export default function BankAccount() {
             <InputLabel className="text-xl" label="Account Number" />
           </div>
           <div className="w-80">
-            <TextInput {...register('account_number')} />
-            <ErrorMessage message={errors?.account_number?.message as string} />
+            <TextInput onChange={handleAccNoChange} value={accNo} />
+            <ErrorMessage message={accountNoValidation?.message} />
           </div>
         </div>
         <div className="flex space-x-10 items-center">
@@ -97,28 +168,32 @@ export default function BankAccount() {
           </div>
           <div className="w-80">
             <select
-              {...register('bank_id')}
+              onChange={handleBankSelection}
               className="w-full h-10 outline-none border border-black rounded cursor-pointer select-none"
             >
               <option value="">select a bank</option>
               {bankList.map((bank) => (
-                <option key={bank.id} value={`${bank.id}`}>
+                <option
+                  key={bank.id}
+                  value={`${bank.id}`}
+                  selected={`${bank.id}` === selectedBank}
+                >
                   {bank.name}
                 </option>
               ))}
             </select>
-            <ErrorMessage message={errors?.bank_id?.message as string} />
+            <ErrorMessage message={bankValidation?.message} />
           </div>
         </div>
         <div className="w-full flex justify-end">
           <div className="w-36">
-            <Button type="submit" onSubmit={handleSubmit(handleFormSubmit)}>
+            <Button type="submit">
               {isLoading ? (
                 <div className="flex items-center justify-center space-x-2 text-sm">
                   <span>Please wait...</span>
                   <Spinner />
                 </div>
-              ) : getValues('account_name') !== '' ? (
+              ) : accName !== '' ? (
                 'Update'
               ) : (
                 'Submit'
--- src/validation/sellerProfileSchema.ts
+++ src/validation/sellerProfileSchema.ts
@@ -1,26 +1,10 @@
 import { z } from 'zod';
 
-export function BankAccountSchema(
-  bankList: { id: string | number; name: string }[]
-) {
-  return z.object({
-    account_name: z
-      .string()
-      .min(1, { message: 'Please enter your account name' }),
-    account_number: z
-      .string()
-      .min(1, { message: 'Please enter your account number' }),
-    bank_id: z.string().refine(
-      (value) => {
-        let isValid = false;
-
-        bankList.forEach((bank) => {
-          if (`${bank.id}` === value) isValid = true;
-        });
-
-        return isValid;
-      },
-      { message: 'Please select a bank' }
-    ),
-  });
-}
+export const BankAccountSchema = z.object({
+  account_name: z
+    .string()
+    .min(1, { message: 'Please enter your account name' }),
+  account_number: z
+    .string()
+    .min(1, { message: 'Please enter your account number' }),
+});
