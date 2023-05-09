4aae1aa682793919bb2d238b19fcfc5d91ef3945
Pratik Neupane <pratikneupane1999@gmail.com>
2023-05-09 18:07:25 +0545

    fix: packging slip

--- src/components/orderManagement/OrdersTable.tsx
+++ src/components/orderManagement/OrdersTable.tsx
@@ -121,6 +121,7 @@ const OrdersTable: React.FC<ProductManagementTableProps> = ({
                     key={i}
                   >
                     {row.cells.map((cell: any, i: number) => {
+                      console.log(row.original)
                       return (
                         <td
                           {...cell.getCellProps()}
@@ -281,6 +282,7 @@ const OrdersTable: React.FC<ProductManagementTableProps> = ({
                                         }
                                         product_name={row.original.product_name}
                                         quantity={row.original.quantity}
+                                        payment_status={row.original.payment_status}
                                       />
                                     </div>
                                     <div
