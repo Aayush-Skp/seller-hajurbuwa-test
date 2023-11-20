f50454cb17d9371970d5b3320c6e8e0cf194415f
Dawood Gul Khan <dawood.gul70@gmail.com>
2023-11-20 09:02:35 +0500

    integrated test environment.

--- src/config/httpClient.ts
+++ src/config/httpClient.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import { ENVIRONMENT_TYPE, ENVIRONMENT_TYPE_TEST } from '../constants/EnvironmentConstant';
 
-const baseURL = 'https://dashboard.hajurbuwa.com/api';
+const baseURL = ENVIRONMENT_TYPE === ENVIRONMENT_TYPE_TEST ? 'http://3.110.177.37/api' : 'https://dashboard.hajurbuwa.com/api';
 
 export const httpClient = axios.create({
   baseURL,
