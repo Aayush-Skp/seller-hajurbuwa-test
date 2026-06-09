d779cfd7378c9a22554936165f336ef307692bec
Krishna Panthi <krishnapanthi94@gmail.com>
2026-06-09 16:58:22 +0545

    added the needed env changes

--- src/config/httpClient.ts
+++ src/config/httpClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { ENVIRONMENT_TYPE, ENVIRONMENT_TYPE_TEST } from '../constants/EnvironmentConstant';
 
-const baseURL = ENVIRONMENT_TYPE === ENVIRONMENT_TYPE_TEST ? 'http://127.0.0.1:8000/api' : 'https://dashboard.hajurbuwa.com/api';
+const baseURL = ENVIRONMENT_TYPE === ENVIRONMENT_TYPE_TEST ? 'http://127.0.0.1:8000/api' : 'https://devdashboard.hajurbuwa.com/api';
 
 export const httpClient = axios.create({
   baseURL,
